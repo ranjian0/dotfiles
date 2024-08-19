54058b9b5abf9aa3b09df05799fbe4a5eb7165ba
ranjian0 <karanjaichungwa@gmail.com>
2024-08-19 19:40:40 +0300

    update lspsaga options

--- .config/nvim/lua/configs/gui.lua
+++ .config/nvim/lua/configs/gui.lua
@@ -2,7 +2,11 @@ local M = {}
 
 function M.config()
   require('noice').setup {}
-  require('lspsaga').setup {}
+  require('lspsaga').setup {
+    lightbulb = {
+      enable = false,
+    }
+  }
 end
 
 return M
