a1d836075f76b630038edc1e8935f7d64b232cbc
ranjian0 <karanjaichungwa@gmail.com>
2026-03-11 17:24:09 +0300

    update aliases

--- .aliases/base.sh
+++ .aliases/base.sh
@@ -6,19 +6,20 @@ alias aptr="sudo apt -y remove"
 alias apts="apt list | fzf --height 60% --reverse --info inline --border"
 alias aptsi="apt list --installed| fzf --height 60% --reverse --info inline --border"
 
-alias pi="uv add"
-alias pu="uv remove"
+#alias pi="uv add"
+#alias pu="uv remove"
 alias pipclean="uv pip freeze | cut -d "@" -f1 | xargs uv pip uninstall -y"
 alias venv="uv venv && source .venv/bin/activate"
 
 alias ni="npm install"
 alias oc="opencode"
 alias ol="ollama"
+
 alias ow="nohup open-webui serve > /dev/null 2>&1 & echo 'Open WebUI started in background.'"
 alias ow-kill="pkill -f 'open-webui serve' && echo 'Open WebUI stopped.'"
+
 alias cc="claude --dangerously-skip-permissions"
 alias cc-gpt="ANTHROPIC_AUTH_TOKEN=ollama ANTHROPIC_BASE_URL=http://localhost:11434 claude --model gpt-oss:20b --dangerously-skip-permissions"
-alias cc-glm-flash="ANTHROPIC_AUTH_TOKEN=ollama ANTHROPIC_BASE_URL=http://localhost:11434 claude --model glm-4.7-flash --dangerously-skip-permissions"
-alias cc-glm-47='ANTHROPIC_AUTH_TOKEN=${ZAI_API_KEY} ANTHROPIC_BASE_URL=https://api.z.ai/api/anthropic API_TIMEOUT_MS=3000000 claude --model glm-4.7 --dangerously-skip-permissions'
+alias cc-glm-5='ANTHROPIC_AUTH_TOKEN=${ZAI_API_KEY} ANTHROPIC_BASE_URL=https://api.z.ai/api/anthropic API_TIMEOUT_MS=3000000 claude --model glm-5 --dangerously-skip-permissions'
 
 alias homelab="ssh ranjian0@192.168.100.25"
